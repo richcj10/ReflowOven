1fabc540f1e0aeee4b523b88e08b8d90542ff329
Richard Johnson <richcj10@gmail.com>
2022-01-24 01:17:25 -0600

    PCB + ArduinoOTA working

--- Code/ReflowOven/src/main.cpp
+++ Code/ReflowOven/src/main.cpp
@@ -8,10 +8,14 @@
 #include "WebService.h"
 #include "Debug.h"
 #include "ReflowControl.h"
+#include <ESP8266mDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
 
 // Replace with your network credentials
 const char* ssid = "Lights.Camera.Action";
 const char* password = "RR58fa!8";
+const char* hostname = "Reflow Oven";
 
 char WiFiStartupTimeout = 0;
 char WiFiTimeout = 0;
@@ -21,16 +25,19 @@ void setup() {
   pinMode(5,OUTPUT);
   DebugSetup(0);
   Serial.begin(115200);
+  //WiFi.config(INADDR_NONE, INADDR_NONE, INADDR_NONE, INADDR_NONE);
+  //String hostname(HOSTNAME);
+  WiFi.hostname(hostname);
   //DisplaySetup();
-  Serial1.println("Boot");
-  TempSetup();
+  // Serial.println("Boot");
+  // TempSetup();
   ProfileSetup();
   if(WifiConfigStatus() == 0){
-      WiFi.softAP("ReflowOven");
+       WiFi.softAP("ReflowOven");
   }
   else{
     WiFi.begin(ssid, password);
-    //WiFi.begin(GetSSID().c_str(), GetSSIDPassword().c_str());
+     //WiFi.begin(GetSSID().c_str(), GetSSIDPassword().c_str());
     while (WiFi.status() != WL_CONNECTED) {
       delay(1000);
       Serial.println(".");
@@ -40,23 +47,59 @@ void setup() {
         break;
       }
     }
-    if(WiFiTimeout == 1){
+  }
+  if(WiFiTimeout == 1){
       Serial.println("AP Mode");
       WiFi.softAP("ReflowOven");
-    }
   }
-  //Serial.println("Ready");
+  Serial.println("Ready");
 
-  // Print ESP32 Local IP Address
+  // // Print ESP32 Local IP Address
   Serial.println(WiFi.localIP());
   WebserviceBegin();
-  //Serial.println("Weppage Started!");
-  Serial.println("Project version: " + String(VERSION));
-  Serial.println("Build timestamp:" + String(BUILD_TIMESTAMP));
-  LoadProfileData(1);
-  GetProfileNames();
-}
+  Serial.println("Weppage Started!");
+  // Serial.println("Project version: " + String(VERSION));
+  // Serial.println("Build timestamp:" + String(BUILD_TIMESTAMP));
+  // //LoadProfileData(1);
+  // //GetProfileNames();
+  // Serial.println(WiFi.localIP());
 
+  ArduinoOTA.onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH) {
+      type = "sketch";
+    } else { // U_FS
+      type = "filesystem";
+    }
+
+    // NOTE: if updating FS this would be the place to unmount FS using FS.end()
+    Serial.println("Start updating " + type);
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) {
+      Serial.println("Auth Failed");
+    } else if (error == OTA_BEGIN_ERROR) {
+      Serial.println("Begin Failed");
+    } else if (error == OTA_CONNECT_ERROR) {
+      Serial.println("Connect Failed");
+    } else if (error == OTA_RECEIVE_ERROR) {
+      Serial.println("Receive Failed");
+    } else if (error == OTA_END_ERROR) {
+      Serial.println("End Failed");
+    }
+  });
+
+  // Start OTA server.
+  ArduinoOTA.setHostname(hostname);
+  ArduinoOTA.begin();
+}
 
 unsigned long ReflowTime = millis();
 unsigned long UxTime = millis();
@@ -64,24 +107,21 @@ unsigned long UxTime = millis();
 char OvenState = 0;
 
 void loop() {
+  ArduinoOTA.handle();
   if((ReflowTime + OVENCONTROLLOOP) < millis()){ //update every 20ms without blocking!
     //TempRead();
-    ReflowTime = millis();
-    if(OvenState == 1){
-      RunProfile(1);
-    }
-    if(OvenState == 0){
-      ReflowStop();
-    }
+    //ReflowTime = millis();
+    //if(OvenState == 1){
+    //  RunProfile(1);
+    //}
+    //if(OvenState == 0){
+    //  ReflowStop();
+    //}
   }
-  if((UxTime + UXCONTROLLOOP) < millis()){ //update every 20ms without blocking!
-    UxTime = millis();
-    //DisplayUpdate();
-  }
-  delay(1000);
-  digitalWrite(5,1);
-  delay(1000);
-  digitalWrite(5,0);
+  // if((UxTime + UXCONTROLLOOP) < millis()){ //update every 20ms without blocking!
+  //   UxTime = millis();
+  //   //DisplayUpdate();
+  // }
 }
 
 
